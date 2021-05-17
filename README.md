dd5f75e198faf4827ad5b9104502a3219d5278da
UdayKiran <uday9139@gmail.com>
2021-05-17 20:04:02 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # VoiceAssistant
-A simple Voice Assistant
+A simple Voice Assistant, whatever you call helps you in your day-day life.
+MADE WITH ❤️ FROM INDIA...
+
+# Note
+Below given aren't commands they're the actual dialogues that should be used to communicate with the Voice Assistant to make your tasks done.
+## Tasks that Assistant is capable to do
+### Opening Youtube (Opens up in a browser)
+speak: "Open Youtube" 
+### Searching for a particular query in youtube (Opens up in a browser)
+speak: "search in Youtube for + 'your search query'"
+### Opening Google (Opens up in a browser)
+speak: "Open Google"
+### Searching Google for a particular query (Opens up in a browser)
+speak: "Search in Google for + 'your search query'"
+### Searching Wikipedia for a particular query (Assistant tells out the serach result.)
+speak: "Search in Wikipedia for + 'your search query'"
+### Taking some care
+speak: "How are you"
+
+
---
+++ main.py
@@ -0,0 +1,100 @@
+# import the required packages...........
+
+import speech_recognition as sr
+import smtplib
+from dotenv import load_dotenv
+import os
+import json
+import wikipedia
+import webbrowser
+from wikipedia.wikipedia import search
+import pyjokes
+from gtts import gTTS
+from playsound import playsound
+import datetime
+
+#Loading all the variables from .env file................
+load_dotenv()
+password = os.getenv("PASS")
+defaultEmail = os.getenv("DEFAULT_EMAIL")
+Email = os.getenv("EMAIL")
+now = datetime.datetime.now()
+assistant = ""
+#Loading the json file..............
+with open("data.json", "r") as File:
+    data = json.load(File)
+#Function which speaks with the master(Simply a tts engine)..............
+def speak(text):
+    myobj = gTTS(text=text, lang='en', slow=False)
+    myobj.save("audio.mp3")
+    playsound('audio.mp3')
+#Function which greets the master..........
+def greet():
+    if (now.hour == 12) and (now.hour <= 4):
+        speak("Good Afternoon!")
+    elif ((now.hour == 0) or (now.hour == 00) and (now.hour < 12)):
+        speak("Good morning!")
+    elif (now.hour >= 16) and (now.hour <= 19):
+        speak("Good evening!")
+    elif (now.hour >= 19) and (now.hour <= 23):
+        speak("Good night!")
+    speak("I'm your personal voice assistant, how may I help you sir?")
+
+#Building a voice-to-text engine's function........
+def takeCommand():
+    microphone = sr.Recognizer()
+    with sr.Microphone() as source:
+        microphone.adjust_for_ambient_noise(source)
+        print("Listening...")
+        #microphone.pause_threshold = 1
+        audio = microphone.listen(source)
+    try:
+        print("Recognizing...")   
+        query = microphone.recognize_google(audio) 
+        print("Master:", query)   
+
+    except Exception as e:
+        print(e)
+        print("Unable to Recognize your voice.") 
+        return "None"
+
+    return query
+
+
+greet()
+#Starting the program's loop.........
+while True:
+    order = takeCommand().lower()
+
+    if "open youtube" == order:
+        speak("Opening Youtube.")
+        webbrowser.open("https://www.google.com/")
+
+    elif "open google" == order:
+        speak("Opening Google.")
+        webbrowser.open("https://www.google.com/")
+
+    elif "wikipedia" in order:
+        query = order.replace("search in wikipedia for", "")
+        results = wikipedia.summary(query, sentences = 3)
+        speak("According to Wikipedia...")
+        print(results)
+        speak(results)
+
+    elif "search in youtube for" in order:
+        query = order.replace("search in youtube for", "")
+        speak("Opening Youtube.")
+        webbrowser.open('https://www.youtube.com/results?search_query=%s' % query)
+
+    elif "search in google for" in order:
+        query = order.replace('search in google for', "")
+        speak("Opening Google.")
+        webbrowser.open("https://www.google.com/search?q=%s" % query)
+
+    elif 'how are you' in order:
+            speak("I am fine, Thank you")
+            speak("How are you, Sir")
+
+    elif 'fine' in order or "good" in order:
+        speak("It's good to know that your fine sir.")
+
